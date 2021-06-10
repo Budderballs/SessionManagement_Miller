8c6b0e64eab3de5cf48c525bbde28942b684ba79
colton chilelli <colton.chilelli@student.centralia.edu>
2021-06-09 20:50:08 -0700

    Add project files.

---
+++ SessionManagement/RFM.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SessionManagement
+{
+    public class RFM : IHttpModule
+    {
+		Dictionary<string, string> t = new Dictionary<string, string>();
+		public void Dispose()
+		{
+			// nothing to dispose
+		}
+
+		public void Init(HttpApplication context)
+		{
+			context.BeginRequest += new EventHandler(OnBeginRequest);
+		}
+
+		private void OnBeginRequest(object sender, EventArgs e)
+		{
+			HttpContext context = ((HttpApplication)sender).Context;
+
+			string URL = context.Request.Path;
+			string user = context.Request.QueryString["u"];
+			string pass = context.Request.QueryString["p"];
+			string encryptUser = string.Empty;
+			string encryptPass = string.Empty;
+
+			if (user != null)
+			{
+				if(!t.ContainsKey(encryptUser))
+                {
+					encryptUser = encrypt(user);
+					encryptPass = encrypt(pass);
+					t.Add(encryptUser, user);
+					t.Add(encryptPass, pass);
+				}
+
+				if(URL.Contains("ThirdPage") && t.Count > 4)
+                {
+					URL = "MembersMain.aspx";
+                }
+
+				context.RewritePath(URL, string.Empty, "u=" + encryptUser + "&p=" + encryptPass, true);
+
+				if(t.ContainsKey(user) && t.ContainsValue(user))
+                {
+					context.RewritePath(URL, string.Empty, "u=" + t[user] + "&p=" + t[pass], true);
+                }
+
+				 else if(t.ContainsValue(user))
+                {
+					context.Response.Redirect(context.Request.Url.ToString());
+                }
+			}
+		}
+
+		public string encrypt(string info)
+        {
+			Random r = new Random(DateTime.Now.Millisecond);
+			Thread.Sleep(5);
+			string salt = r.Next(100000, 999999).ToString();
+			int a;
+			byte[] b;
+			byte[] c;
+			b = ASCIIEncoding.ASCII.GetBytes(info);
+			c = new MD5CryptoServiceProvider().ComputeHash(b);
+			StringBuilder output = new StringBuilder(c.Length);
+			for(a=0; a < c.Length; a++)
+            {
+				output.Append(c[a].ToString("X2"));
+            }
+			return output.ToString().Substring(0, 8) + salt + output.ToString().Substring(8);
+        }
+	}
+}
---
+++ SessionManagement/ThirdPage.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ThirdPage.aspx.cs" Inherits="SessionManagement.ThirdPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblUser" runat="server" Font-Size="Large" />
+            <div style="float:right;width:300px;padding:5px; text-align:right;">
+                    <asp:Button ID="btnLogIn" runat="server" Text="Log In" OnClick="btnLogIn_Click" />
+                </div>
+        </div>
+    </form>
+</body>
+</html>
